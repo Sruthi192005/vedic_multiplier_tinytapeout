c0624c4098294b570945c4cf2ca57df911a3c4e0
Sruthi192005 <220101131083@cutm.ac.in>
2025-07-28 22:12:32 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,30 +1,33 @@
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
 
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
-import random
+
 
 @cocotb.test()
 async def test_project(dut):
+    dut._log.info("Start")
+
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
     dut.ena.value = 1
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 5)
+    await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    for _ in range(10):
-        a = random.randint(0, 15)
-        b = random.randint(0, 15)
+    dut.ui_in.value = (3 << 4) | 2
+    await ClockCycles(dut.clk, 2)
+    assert dut.uo_out.value == 6, f"Expected 6, got {dut.uo_out.value.integer}"
 
-        # Format: lower 4 bits = a, upper 4 bits = b
-        dut.ui_in.value = (b << 4) | a
-        await ClockCycles(dut.clk, 1)
+    dut.ui_in.value = (5 << 4) | 4
+    await ClockCycles(dut.clk, 2)
+    assert dut.uo_out.value == 20, f"Expected 20, got {dut.uo_out.value.integer}"
 
-        expected = a * b
-        actual = dut.uo_out.value.integer
-
-        assert actual == expected, f"{a} * {b} = {expected}, got {actual}"
+    dut.ui_in.value = (15 << 4) | 15
+    await ClockCycles(dut.clk, 2)
+    assert dut.uo_out.value == 225, f"Expected 225, got {dut.uo_out.value.integer}"
